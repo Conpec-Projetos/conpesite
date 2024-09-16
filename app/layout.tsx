6d13c1435d3e2e3a778b66d0ae974afeccacf301
Felipe Silva <felipe.silva@conpec.com.br>
2024-09-16 17:12:13 -0300

    feat: adicionar CSS global + fontes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,24 @@
+import { Poppins, Lato } from 'next/font/google'
+
+import './global.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  display: 'swap',
+  weight: ['400', '600', '700'],
+})
+
+const lato = Lato({
+  subsets: ['latin'],
+  variable: '--font-lato',
+  display: 'swap',
+  weight: ['400', '700'],
+})
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html>
+    <html lang="pt-BR" className={`${poppins.variable} ${lato.variable}`}>
       <body>
         {children}
       </body>
