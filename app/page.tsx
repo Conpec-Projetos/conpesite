d85250c61cb54df73259be809c266d7f3175ace2
Felipe Silva <felipe.silva@conpec.com.br>
2024-10-02 10:46:43 -0300

    feat(homepage): adicionar seção Sobre

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image'
+import imagemEpej from '@/assets/epej.jpg'
+
+import style from './page.module.css'
+
 export default function Home() {
-  return <></>
+  return (
+    <main>
+      <section className={style.sectSobre}>
+        <Image
+          src={imagemEpej}
+          alt="Membros da Conpec estendendo a bandeira da empresa no evento EPEJ."
+        />
+        <div>
+          <h2>O que é a <span>Conpec</span>?</h2>
+          <p>
+            Criada em 1991, a Conpec é a Empresa Júnior dos cursos de Computação da Unicamp,&nbsp;
+            <b>os melhores do Brasil</b>.
+
+            Somos uma software house focada no <b>sucesso dos nossos clientes</b>, entregando sempre
+            a <b>melhor solução</b> para você.
+          </p>
+        </div>
+      </section>
+    </main>
+  )
 }
