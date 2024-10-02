6765ae43c579e3b8f06f07d2ab0c4edd968dc5b0
Felipe Silva <felipe.silva@conpec.com.br>
2024-10-01 21:33:43 -0300

    feat: adicionar componente `Button`
    O botão `.white` (saiba mais) não está fiel a proto, mas novamente, acho
    importante manter assim por questões de acessibilidade.
    
    O mesmo acontece com os outros botões também, na verdade, em especial no
    `:hover`, mas é menos pior.

---
+++ components/Button.tsx
@@ -0,0 +1,32 @@
+import type { MouseEventHandler, ReactNode } from 'react'
+
+import styles from './button.module.css'
+import Link from 'next/link'
+
+interface BaseProps {
+  children: ReactNode
+  theme: 'fill' | 'outline' | 'tiny' | 'white'
+}
+
+interface LinkProps extends BaseProps {
+  href: string
+}
+
+interface ButtonProps extends BaseProps {
+  href?: never
+  onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+type Props = LinkProps | ButtonProps
+
+// Esse botão que pode ser usado tanto para links quanto como um `<button>` mesmo. Tudo está
+// definido no TypeScript acima, que, na prática, constrói dois tipos diferentes de `Props`
+// extendendo um objeto base, que tem elementos comuns à ambos, e então usa a união desses valores.
+// Moral da história: tudo que não tem `href` é um `<button>`, e o `onClick` só pode ser utilizado
+// em `<button>`. Não tem como mesclar as propriedades dos dois.
+export default function Button(props: Props) {
+  if (props.href === undefined) {
+    return <button className={styles[props.theme]} onClick={props.onClick}>{props.children}</button>
+  }
+  return <Link className={styles[props.theme]} href={props.href}>{props.children}</Link>
+}
