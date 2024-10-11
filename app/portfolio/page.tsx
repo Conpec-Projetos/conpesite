d5391a6889a4cae685d74680309deea24c8a7155
Gustavo Borba <gustavo.borba2001@gmail.com>
2024-10-11 16:30:42 -0300

    feat: painel categoria feito

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,8 @@
 import styles from './portfolio.module.css'
 import Image from 'next/image'
 import logosClientes from '/app/assets/Logos.png'
+import logoImunolab from '/app/assets/Imunolab.png'
+import mockups from '/app/assets/Mockups.png'
 
 export default function Portfolio() {
   return (
@@ -24,24 +26,47 @@ export default function Portfolio() {
           <div className={`${styles.textDiv} ${styles.right}`}>
             <p className={styles.subtitle}>CONHEÇA OS</p>
                 <h1 className={styles.titles}>NOSSOS CLIENTES</h1>
-                <p className={styles.text}>Trabalhamos arduamente para entregar um produto de qualidade,
+                <p className={`${styles.text} ${styles.head}`}>Trabalhamos arduamente para entregar um produto de qualidade,
                   baseado em tecnologias de ponta. Conheça alguns de nossos projetos e entenda como 
                   ajudamos nossos clientes a tornar seus projetos realidade.</p>
           </div>
       </div>
+
       <div className={styles.mediaDiv}>
         <div className={styles.textDiv}>
-          <h1 className={styles.titles}>IMUNOLAB</h1>
-          <p className={styles.text}>Trabalhamos arduamente para entregar um produto de qualidade,
-            baseado em tecnologias de ponta. Conheça alguns de nossos projetos e entenda como 
-            ajudamos nossos clientes a tornar seus projetos realidade.</p>
+          <div className={styles.rowContainerDiv}>
+            <div className={styles.categoryDiv}>
+              Prototipação
+            </div>
+            <div className={styles.categoryDiv}>
+              Desenvolvimento
+            </div>  
+          </div>
+          <div className={styles.rowContainerDiv}>
+            <div>
+              <Image
+              src={logoImunolab}
+              alt='Logo da empresa Imunolab'
+              width={55}
+              ></Image>  
+            </div>
+            
+            <div>
+              <h1 className={styles.titles}>IMUNOLAB</h1>
+            </div>
+          </div>
+            
+          <p className={`${styles.text} ${styles.body}`}>Como parte de uma pesquisa <b>FISEX—Unicamp</b>,
+            especialistas contrataram a <b>Conpec </b> para a prototipação e
+            desenvolvimento de um website e aplicativo que ajude no
+            acompanhamento físico de atletas.</p>
 
         </div>
         <div className={styles.imageDiv}>
           <Image
-            src={logosClientes}
+            src={mockups}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            alt='Imagem com os logos dos clientes passados da Conpec'
+            alt='Imagem Mockups em um Iphone das telas do aplicativo'
             layout='responsive'
             width={438}
             height={349}
@@ -94,9 +119,8 @@ export default function Portfolio() {
             
           </Image>
         </div>
-          
-          
       </div>
+
       <div className={styles.mediaDiv}>
         <div className={styles.imageDiv}>
           {/* FAZER A IMAGEM IR PRA CIMA QUANDO MIN-WIDTH */}
@@ -119,6 +143,10 @@ export default function Portfolio() {
                   baseado em tecnologias de ponta. Conheça alguns de nossos projetos e entenda como 
                   ajudamos nossos clientes a tornar seus projetos realidade.</p>
           </div>
+      </div>
+
+      <div>
+        Negocio rodando
       </div>
       
       
