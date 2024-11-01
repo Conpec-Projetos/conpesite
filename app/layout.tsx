9bcc7e027b4868494520ee83615d69d21b4eaa0c
Pfeiffer <vitor.pfeiffer@conpec.com.br>
2024-11-01 17:04:27 -0300

    feat(homepage): adicionar seção de relatos
    Signed-off-by: Felipe Silva <felipe.silva@conpec.com.br>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Poppins, Lato } from 'next/font/google'
+import { Poppins, Lato, DM_Serif_Text } from 'next/font/google'
 
 import './global.css'
 import Footer from '@/components/Footer'
@@ -17,9 +17,16 @@ const lato = Lato({
   weight: ['400', '700'],
 })
 
+const dm_serif = DM_Serif_Text({
+  subsets: ['latin'],
+  variable: '--font-dm-serif',
+  display: 'swap',
+  weight: ['400'],
+})
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-BR" className={`${poppins.variable} ${lato.variable}`}>
+    <html lang="pt-BR" className={`${poppins.variable} ${lato.variable} ${dm_serif.variable}`}>
       <body>
         {children}
         <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Link from 'next/link'
 import ConsultoriaIcon from '@/components/svg/ConsultoriaIcon'
 import ProtoIcon from '@/components/svg/ProtoIcon'
 import DevIcon from '@/components/svg/DevIcon'
+import Aspas from '@/components/svg/Aspas'
 
 export default function Home() {
   return (
@@ -154,6 +155,37 @@ export default function Home() {
               de acordo com a sua ideia original.
             </p>
           </div>
+        </div>
+      </section>
+      <section className={style.sectRelatos}>
+        <div className={style.relatos}>
+          <div>
+            <Aspas aria-hidden="true" />
+            <blockquote>
+              Cotamos um trabalho para desenvolvimento do aplicativo e a Conpec foi a que apresentou a melhor proposta e cumpriu com a melhor solução.
+              O aplicativo em si vem dando os resultado que a gente esperava e muito mais. Além de todo a dedicação da equipe em trabalhar conosco, seja
+              realizando as atividades, seja nos dando atenções e orientações, para que a gente consiga desenvolver o nosso trabalho por aqui. Então,
+              eu queria agradecer muito o profissionalismo e dedicação da Conpec.
+            </blockquote>
+            <cite>Murilo Martins, Finança Médica</cite>
+          </div>
+          <div>
+            <Aspas aria-hidden="true" />
+            <blockquote>
+              Minha experiência com o trabalho da Conpec foi excelente, não posso descrever de outra forma. Acredito que a Conpec não só disponibilizou as
+              melhores pessoas para atender o desafio que tínhamos na Cargill, mas convocou uma equipe transparente e eficiente no que se dispunha a entregar.
+              Realmente acredito que trabalhos como esse exaltam o potencial de entrega que o MEJ tem como um todo e, pelo meu lado, foi um prazer muito grande
+              ter participado desse projeto com o time da Conpec que, sem sombra de dúvidas, entregou muito valor para a gente.
+            </blockquote>
+            <cite>Matheus Gomes, representante da Cargill</cite>
+          </div>
+        </div>
+        <div className={style.cta}>
+          <hgroup>
+            <p>Nós temos a solução para você</p>
+            <h2>Veja como podemos <span>te ajudar</span>.</h2>
+          </hgroup>
+          <Button href="/contato" theme="fill">Agende um diagnóstico</Button>
         </div>
       </section>
     </main>
