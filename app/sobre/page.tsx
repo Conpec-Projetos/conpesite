c3f6d10d1341cf0751b98a04633ef184692903f9
Felipe Silva <felipe.silva@conpec.com.br>
2024-11-08 16:22:53 -0300

    feat(sobre): adicionar seção MVV

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import ImagemAlvorada from '@/assets/alvorada.jpg'
 
 import style from './page.module.css'
+import { mvv } from '@/constants'
 
 export default function Sobre() {
   return (
@@ -15,6 +16,16 @@ export default function Sobre() {
           </p>
         </div>
         <Image src={ImagemAlvorada} alt="Membros da Conpec posam para foto no evento regional Alvorada." />
+      </section>
+      <section className={style.sectMVV}>
+        <div>
+          <h2>Temos um <strong>propósito</strong>.</h2>
+          <p>{mvv.missao}</p>
+        </div>
+        <div>
+          <h2>Temos uma <strong>visão do futuro</strong>.</h2>
+          <p>{mvv.visao}</p>
+        </div>
       </section>
     </main>
   )
--- constants.ts
+++ constants.ts
@@ -1,8 +1,13 @@
 export const social = {
-  whatsapp: "(21) 96825-3902",
-  whatsappRaw: "5521968253902",
-  email: "contato@conpec.com.br",
-  instagram: "conpec.ej"
+  whatsapp: '(21) 96825-3902',
+  whatsappRaw: '5521968253902',
+  email: 'contato@conpec.com.br',
+  instagram: 'conpec.ej',
 }
 
-export const address = "R. Josué de Castro, 100 — Cidade Universitária, Campinas — SP, 13083-861"
+export const address = 'R. Josué de Castro, 100 — Cidade Universitária, Campinas — SP, 13083-861'
+
+export const mvv = {
+  missao: 'Desenvolver membros autônomos e inovadores capazes de transformar ideias em soluções tecnológicas impactantes.',
+  visao: 'Ser uma EJ formada por membros de destaque que nos torne referência nacional.',
+}
