9985a21e05f0df22157115d51f413c5db6a5a4f9
Felipe Silva <felipe.silva@conpec.com.br>
2025-01-10 16:10:13 -0400

    feat(form): enviar mensagens do form via Slack

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ O protótipo está no [Figma](https://www.figma.com/design/XwVQnNcbmC0xRJv4eOKuh
 - `npm run dev`: roda o servidor de desenvolvimento
 - `npm run lint && npm run build`: verifica se não nem nenhum erro _(execute antes de dar commit/push!)_
 
+## Configuração
+O website precisa de algumas variáveis de ambiente para funcionar corretamente. As variáveis podem ser adicionadas pelo arquivo `.env.local`, em caso de desenvolvimento. Cuidado para não enviar as credenciais para o Github...
+
+- `SLACK_TOKEN`: OAuth Token de um bot do Slack. É específico para cada _workspace_.
+- `SLACK_CHANNEL`: ID do canal a serem enviadas as mensagens do formulário. Note que o bot precisa ser adicionado ao canal para que funcione.
+
 ## Layout geral
 - `/`
 - `/sobre`
--- components/Form.tsx
+++ components/Form.tsx
@@ -6,10 +6,42 @@ import style from './form.module.css'
 import bgImage from '@/assets/forms_bg.jpg'
 import Button from './Button'
 
+async function sendSlack(formData: FormData) {
+  const ideia = formData.get('ideia')?.toString().replaceAll('\n', '\n> ') // use blockquote for textarea
+  const message = `
+  *_Nova mensagem do Conpesite_*
+  • *Nome*: ${formData.get('nome')}
+  • *Telefone*: ${formData.get('tel')}
+  • *Email*: ${formData.get('email')}
+  • *Como conheceu*: ${formData.get('referrer')}
+  • *Orçamento*: a partir de R$${formData.get('orçamento')},00
+  • *Ideia*:
+  > ${ideia}
+`;
+  const response = await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    body: JSON.stringify({
+      channel: process.env.SLACK_CHANNEL,
+      text: message
+    }),
+    headers: {'Content-Type':'application/json', 'Authorization': 'Bearer ' + process.env.SLACK_TOKEN }
+  });
+
+  const response_json = await response.json();
+
+  if (!response_json.ok) {
+    throw new Error('Slack: ' + response_json.error)
+  }
+}
+
 async function handleForm(formData: FormData) {
   'use server'
 
-  // enviar forms. em caso de erro, redirecionar para `/enviado?error=erro`
+  try {
+    sendSlack(formData)
+  } catch (e) {
+    if (e instanceof Error) redirect('/enviado?error=' + e.message)
+  }
 
   redirect('/enviado')
 }
